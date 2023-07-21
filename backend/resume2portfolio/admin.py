a5a5a017c7eb7e2a54880c437ae6e02289969d5a
dixitdeeksha <dixitd510@gmail.com>
2023-07-21 16:58:16 +0530

    models created

--- backend/resume2portfolio/admin.py
+++ backend/resume2portfolio/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
+from .models import User, Bio, TechnicalSkills, QualificationDetails, Projects
+# admin.site.register(User)
+admin.site.register(Bio)
+admin.site.register(TechnicalSkills)
+admin.site.register(QualificationDetails)
+admin.site.register(Projects)
 
 
-from .models import user
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('name', 'bio')
+    list_display = ('Name', 'LinkedInURL')
 
 # Register your models here.
 
-admin.site.register(user, UserAdmin)
+admin.site.register(User, UserAdmin)
--- backend/resume2portfolio/models.py
+++ backend/resume2portfolio/models.py
@@ -1,13 +1,56 @@
 from django.db import models
 
-# Create your models here.
+# from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    # Django automatically creates 'id' field as the primary key
+    Username = models.CharField(max_length=100,null=True)
+    Name = models.CharField(max_length=100, null=True)
 
-class user(models.Model):
-    name = models.CharField(max_length=120)
-    bio= models.TextField()
-    # completed = models.BooleanField(default=False)
+    Email = models.EmailField(default='', unique=True)
+    # Password = models.CharField(max_length=100)
+    LinkedInURL = models.URLField(blank=True, null=True)
+    GitHubURL = models.URLField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.Username
 
-    def _str_(self):
-        return self.name
+class Bio(models.Model):
+    BioID = models.AutoField(primary_key=True)
+    UserID = models.OneToOneField(User, on_delete=models.CASCADE, related_name='bio')
+    Role = models.CharField(max_length=100)
+    AboutMe = models.TextField()
+    
+    def __str__(self):
+        return f"{self.UserID.Username}'s Bio"
+
+class TechnicalSkills(models.Model):
+    SkillID = models.AutoField(primary_key=True)
+    UserID = models.OneToOneField(User, on_delete=models.CASCADE, related_name='technical_skills')
+    CodingLanguages = models.TextField()
+    OtherTechnologies = models.TextField()
+    
+    def __str__(self):
+        return f"{self.UserID.Username}'s Technical Skills"
+
+class QualificationDetails(models.Model):
+    QualificationID = models.AutoField(primary_key=True)
+    UserID = models.OneToOneField(User, on_delete=models.CASCADE, related_name='qualification_details')
+    DegreeName = models.CharField(max_length=100)
+    YearFrom = models.PositiveIntegerField()
+    YearTo = models.PositiveIntegerField()
+    CGPA = models.FloatField()
+    CollegeSchoolName = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return f"{self.UserID.Username}'s Qualification Details"
+
+class Projects(models.Model):
+    ProjectID = models.AutoField(primary_key=True)
+    UserID = models.ForeignKey(User, on_delete=models.CASCADE, related_name='projects')
+    ProjectTitle = models.CharField(max_length=200)
+    ProjectLink = models.URLField(blank=True, null=True)
+    ProjectDescription = models.TextField()
+    
+    def __str__(self):
+        return f"{self.UserID.Username}'s Project: {self.ProjectTitle}"
